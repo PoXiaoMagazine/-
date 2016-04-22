963b641c19b3964b0859581e28954e0f577a67b7
Martin <nielumartin@hotmail.com>
2016-04-22 21:42:08 +0800

    add

---
+++ script/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        //sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', 'whitesmoke', '#000'],
+        anchors:['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15'],
+        //'slidesNavigation': true,
+        'scrollOverflow': true,
+        'controlArrows': false,
+    });
+});
